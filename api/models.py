9829a623ea637671066a057b00b3749fe96e4ed3
GhiaC <ghiasy@aut.ac.ir>
2018-06-22 02:06:11 +0430

    change database

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+# from pygments.lexers import get_all_lexers
+# from pygments.styles import get_all_styles
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- rest/urls.py
+++ rest/urls.py
@@ -1,22 +1,18 @@
-"""rest URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+from django.conf.urls import url, include
+from rest_framework import routers
+from api import views
 from django.conf.urls import include, url
 from django.contrib import admin
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^api/', include('api.url')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api/', include('api.url')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
